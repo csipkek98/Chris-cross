909e5bed12f75f5d6a806002b071e9fa6bc8778c
SentexX <csipkekrisztian@gmail.com>
2022-11-22 15:39:57 +0100

    Modify: socket.io import method

--- backend/main.js
+++ backend/main.js
@@ -1,13 +1,18 @@
+// import Board from './board';
+import Board from './board.js';
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-const io = require('socket.io')({
+const httpServer = createServer();
+const io = new Server(httpServer, {
     cors: {
         origin: "*",
         methods: ["GET","POST"]
     }
 });
 
-io.listen(3000);
+httpServer.listen(5000);
 
 io.on('connection', client => {
-     console.log("NEW CLIENT CONNECTED: "+client.id) 
+    console.log("NEW CLIENT CONNECTED: "+client.id) 
     });
